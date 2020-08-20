1050b38fff0c2644d67be757530a72a52fa2a327
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-20 22:10:40 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -6,9 +6,13 @@ class Book
   def initialize(title)
     @title = title
   end
-
   def turn_page
     puts "Flipping the page...wow, you read fast!"
+  end
+
+  def genre= (genre)
+    @genre = genre
+    GENRES << genre
   end
 
 end
